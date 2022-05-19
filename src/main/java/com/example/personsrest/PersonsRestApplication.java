ff1d2c3112516f6a99842640bbae8fb05490e0db
Omar Nasser <55090516+Omanas98@users.noreply.github.com>
2022-05-19 13:46:30 +0200

    More comments

--- src/main/java/com/example/personsrest/PersonsRestApplication.java
+++ src/main/java/com/example/personsrest/PersonsRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+// @ComponentScan makes sure to scan and go through other specified Packages
 @SpringBootApplication
 @ComponentScan({"com.example.config"})
 public class PersonsRestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PersonsRestApplication.class, args);
     }
